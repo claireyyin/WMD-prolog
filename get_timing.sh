b5efa172014c4e9e7dc1dafd8d7653718da62ca5
Claire <107508634+claireyyin@users.noreply.github.com>
2024-05-30 19:28:15 -0500

    Update get_timing.sh

--- get_timing.sh
+++ get_timing.sh
@@ -2,7 +2,7 @@
 # Get find_person1() query timings; run 10 times
 echo "Timing started"
 # Start Prolog interpreter and write to txt file
-swipl -s main.pl <<EOF > time10.txt 2>&1
+swipl -s singlequery.pl <<EOF > time10.txt 2>&1
 repeat_time_query(10). 
 halt.
 EOF
